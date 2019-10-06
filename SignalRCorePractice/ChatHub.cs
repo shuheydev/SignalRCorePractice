5e7b4a44cb3bd88233f86248b249c409a239a03d
shuhey <nishizawa@timedia.co.jp>
2019-10-06 16:46:45 +0900

    Add project files.

---
+++ SignalRCorePractice/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRCorePractice
+{
+    internal class ChatHub:Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.SendAsync("Send", message);
+        }
+    }
+}
