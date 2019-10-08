16cdcb97d9f9cc24039ccc85ba63d3777c6d2c34
shuhey <nishizawa@timedia.co.jp>
2019-10-08 16:37:47 +0900

    切断ボタンを追加.接続に関するイベントハンドラを追加.

--- SignalRClientWpf/Views/MainWindow.xaml.cs
+++ SignalRClientWpf/Views/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SignalRClientWpf
 {
@@ -30,15 +19,13 @@ namespace SignalRClientWpf
             //接続のためのHubConnectionのインスタンスを作成する.
             connection = new HubConnectionBuilder()
                 .WithUrl(@"https://localhost:44350/chathub")//サーバー側でMapHubで指定したURLを指定する.
+                .WithAutomaticReconnect()
                 .Build();
-        }
 
-        //接続ボタンが押されたときの処理
-        private async void connectButton_Click(object sender, RoutedEventArgs e)
-        {
             //サーバー側から呼び出される処理を定義
             //サーバー側からReceiveを指定して呼び出しがあったときに,
             //登録したDelegateが実行される.
+            //複数回これを実行すると複数登録されるので注意.
             connection.On<string>("Receive", (message) =>
             {
                 //Dispatcher使っている理由は?
@@ -50,6 +37,40 @@ namespace SignalRClientWpf
                 });
             });
 
+
+            //接続状態のイベント
+            connection.Closed += (message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    messagesList.Items.Add($"Closed::");
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnecting += (message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    messagesList.Items.Add($"Reconnecting::{message.Message}");
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += (message) => {
+                this.Dispatcher.Invoke(() => {
+                    messagesList.Items.Add($"Reconnected::{message}");
+                });
+
+                return Task.CompletedTask;
+            };
+        }
+
+        //接続ボタンが押されたときの処理
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
             try
             {
                 //接続
@@ -71,6 +92,19 @@ namespace SignalRClientWpf
                 //サーバー側のSendメソッドを呼び出し,TextBoxの文字列を送る.
                 //サーバー側のSendメソッドは2つのstringを受け取る
                 await connection.InvokeAsync("send", messageTextBox.Text, "WPF");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void disconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.StopAsync();
+                connection.Remove("Receive");
             }
             catch (Exception ex)
             {
