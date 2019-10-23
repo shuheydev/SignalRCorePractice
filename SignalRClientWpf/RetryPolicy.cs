bf31b7b12d6548e7cbdc047de05f4a44829c15bc
shuheydev <shuhey.dev@gmail.com>
2019-10-23 20:45:23 +0900

    Add retry policy

---
+++ SignalRClientWpf/RetryPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRCorePractice
+{
+    public class RandomRetryPolicy : IRetryPolicy
+    {
+        private readonly Random _random = new Random();
+
+        public TimeSpan? NextRetryDelay(RetryContext retryContext)
+        {
+            //2~5秒の間でランダムに再接続を試みる
+            return TimeSpan.FromSeconds(_random.Next(2,5));
+        }
+    }
+}
