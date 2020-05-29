c6ed6c0ffed02ea19c360e53db1330dd7d5a5e14
Shuhey <shuhey.dev@gmail.com>
2020-05-30 02:44:30 +0900

    commit

--- SignalRCorePractice/Startup.cs
+++ SignalRCorePractice/Startup.cs
@@ -12,8 +12,6 @@ namespace SignalRCorePractice
 {
     public class Startup
     {
-        private string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,11 +25,11 @@ namespace SignalRCorePractice
                         //       .AllowAnyHeader()
                         //       .AllowAnyMethod()
                         //       .AllowCredentials();
-                        builder.SetIsOriginAllowed(origin=>true)
+                        builder.SetIsOriginAllowed(origin => true)
                                .AllowAnyHeader()
                                .AllowAnyMethod()
                                .AllowCredentials();
-                        });
+                    });
             });
 
             services.AddSignalR();
